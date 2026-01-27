3ae19eb354faebec043ae826172f4a2e54d54a2e
Satyam Raj <satyamrajgaya434@gmail.com>
2026-01-27 20:34:30 +0530

    Enhance lock screen layout and functionality with profile picture, system icons, and improved styles

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Set time and date on lock screen
 document.getElementById("time").innerText = new Date().toLocaleTimeString([], {
   hour: "2-digit",
   minute: "2-digit",
@@ -12,3 +13,16 @@ document.getElementById("day-date").innerText = new Date().toLocaleDateString(
     day: "numeric",
   },
 );
+
+// From Lock Home Screen to Lock Screen
+const lockHomeScreen = document.getElementById("lock-home-screen");
+const lockScreen = document.getElementById("lock-screen");
+
+const goToLockScreen = () => {
+  lockHomeScreen.classList.add("active");
+  lockScreen.classList.add("active");
+};
+
+lockHomeScreen.addEventListener("click", (e) => {
+  if (!e.target.closest(".card, a")) goToLockScreen();
+});
