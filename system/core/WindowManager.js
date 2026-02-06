5283f484c524845a94a60d9b551604803e266e7a
Satyam Raj <satyamrajgaya434@gmail.com>
2026-02-06 19:16:28 +0530

    Enhance window controls with SVG icons and improve main layout styling

--- system/core/WindowManager.js
+++ system/core/WindowManager.js
@@ -125,9 +125,9 @@ export class WindowManager {
           <span class="title-text">${app.name}</span>
         </div>
         <div class="controls">
-          <button class="min">&minus;</button>
-          <button class="max">&#9633;</button>
-          <button class="close">&times;</button>
+          <button class="min"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minus-icon lucide-minus"><path d="M5 12h14"/></svg></button>
+          <button class="max"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-icon lucide-square"><rect width="18" height="18" x="3" y="3" rx="2"/></svg></button>
+          <button class="close"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg></button>
         </div>
       </div>
       
